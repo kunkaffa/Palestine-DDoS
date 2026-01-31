6bf2d8fc8cf39840ef3091ff4f613bff91bccbce
kunkaffa <kunkaffa3@gmail.com>
2026-01-31 12:37:16 +0700

    Update output messages in ghoza.py

--- ghoza.py
+++ ghoza.py
@@ -65,7 +65,7 @@ def countdown(t):
             stdout.write(f"\r{Fore.BLUE}| [*]{Fore.RED} {remaining_time:.2f} {Fore.BLUE} Sec left{' ' * 26}|")
             print(f"\r💥 {Fore.LIGHTBLUE_EX} {url} {Fore.BLUE}Sec left{Fore.RED} {remaining_time:.2f}")
             print(f"\r☠️ {Fore.YELLOW} {url} {Fore.BLUE} {rate_limit}")
-        
+            print(f"\r🚀 {Fore.WHITE} {url} {Fore.CYAN} request {request}")
         else:
             stdout.flush()
             stdout.write(f"\r{Fore.RED}[÷] {Fore.CYAN} Attack has been completed|\n")
@@ -165,7 +165,8 @@ async def run_stress_test(url: str, duration: int, concurrency: int,
                 stdout.flush()
                 # Tambahkan gaya progress seperti countdown asli
                 print(f"\r💥 {Fore.LIGHTBLUE_EX} {url} {Fore.BLUE}Sec left{Fore.RED} {remaining:6.2f}")
-                print(f"\r☠️ {Fore.YELLOW} {url} {Fore.GREEN} {rate_limit}")
+                print(f"\r☠️ {Fore.YELLOW} {url} {Fore.GREEN}r_limit {Fore.MAGENTA} {rate_limit}")
+                print(f"\r🚀 {Fore.WHITE} {url} {Fore.CYAN} request {request}")
                 await asyncio.sleep(0.5)
 
         prog_task = asyncio.create_task(progress())
