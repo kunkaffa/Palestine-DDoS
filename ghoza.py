4bb8346736dbe897a4424922416bd7c436be0f05
kunkaffa <kunkaffa3@gmail.com>
2026-01-31 13:11:15 +0700

    Refactor progress output formatting in ghoza.py

--- ghoza.py
+++ ghoza.py
@@ -164,9 +164,9 @@ async def run_stress_test(url: str, duration: int, concurrency: int,
                 stdout.write(f"\r{Fore.BLUE}| [*]{Fore.RED} {remaining:6.2f} {Fore.BLUE} Sec left{' ' * 26}|")
                 stdout.flush()
                 # Tambahkan gaya progress seperti countdown asli
-                print(f"\r💥 {Fore.LIGHTBLUE_EX} {url} {Fore.BLUE}Sec left{Fore.RED} {remaining:6.2f}")
-                print(f"\r☠️ {Fore.WHITE}{url} {Fore.GREEN} r_limit {Fore.MAGENTA} {rate_limit}")
-                print(f"\r🚀 {Fore.YELLOW}{asyncio.run} {Fore.CYAN} methods {method}")
+                print(f"\r💥{Fore.LIGHTBLUE_EX} {url} {Fore.BLUE}Sec left{Fore.RED} {remaining:6.2f}")
+                print(f"\r☠️{Fore.WHITE} {url} {Fore.GREEN}r_limit {Fore.MAGENTA} {rate_limit}")
+                print(f"\r🚀{Fore.YELLOW} {asyncio.run} {Fore.CYAN}methods {method}")
                 await asyncio.sleep(0.5)
 
         prog_task = asyncio.create_task(progress())
